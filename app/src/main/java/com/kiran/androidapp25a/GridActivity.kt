b8dcbd11e53cbbb9dc55d83fee24bc7d4742df0c
Kiran Rana <kiranrana8973@gmail.com>
2020-12-06 22:11:29 +0545

    test

---
+++ app/src/main/java/com/kiran/androidapp25a/GridActivity.kt
@@ -0,0 +1,14 @@
+package com.kiran.androidapp25a
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GridActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_grid)
+
+        val a : Int
+    }
+}
+//asdasda
